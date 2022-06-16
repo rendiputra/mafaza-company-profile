5d4d67faf21e4de2d9ae351879ee7b49f13903be
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2022-06-16 15:48:00 +0700

    update: kegiatan dan layanan menjadi dinamis

--- config.php
+++ config.php
@@ -4,10 +4,8 @@ $database = "db_mafaza";
 $username = "root";
 $password = "";
  
-// untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
-// membuat koneksi
 $mysqli = new mysqli($servername, $username, $password ,$database);
-// mengecek koneksi
+// check connection to database
 if ($mysqli->connect_errno) {
     die("Connection failed: " . mysqli_connect_error());
     exit();
